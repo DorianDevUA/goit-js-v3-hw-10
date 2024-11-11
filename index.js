3a4980766905fdea5c83a2d62bb445e35809a871
DorianDevUA <147657298+DorianDevUA@users.noreply.github.com>
2024-11-11 11:27:27 +0000

    Deploying to gh-pages from @ DorianDevUA/goit-js-v3-hw-10@840431c0395021f6744ff3ee124832850c390a8c 🚀

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                     */console.log("Hello world");const o={rootContainer:document.querySelector("#root")};o.rootContainer.innerHTML="<div>This is Homepage!</div>";
+import"./assets/style-NG-l__dc.js";console.log("Hello world");const o={rootContainer:document.querySelector("#root")};o.rootContainer.innerHTML="<div>This is Homepage!</div>";
 //# sourceMappingURL=index.js.map
